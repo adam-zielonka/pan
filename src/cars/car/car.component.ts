515449eba39bfd93f21700ec941d766058d1293a
Adam Zielonka <root@adamzielonka.pro>
2018-06-09 19:41:33 +0200

    Updated parking serfice for add, delete and update cars

--- src/cars/car/car.component.ts
+++ src/cars/car/car.component.ts
@@ -10,13 +10,53 @@ import { ParkingService } from '../parking.service';
 export class CarComponent implements OnInit {
 
   cars : Pojazd[]
+  car : Pojazd
+  editCar : Pojazd
 
   constructor(
     private parkingService : ParkingService
   ) { }
 
   ngOnInit() {
+    this.editCar = new Pojazd()
     this.parkingService.getCars().subscribe(cars => this.cars = cars)
+  }
+
+  editMode(car : Pojazd) {
+    this.car = car
+    this.editCar = Object.assign({}, car)
+  }
+
+  isEdit(car : Pojazd) {
+    if(!this.editCar) return false
+    return car._id == this.editCar._id
+  }
+
+  afterEnd() {
+    for (const key in this.car)
+      if (this.car.hasOwnProperty(key))
+        this.car[key] = this.editCar[key]
+    this.editCar._id = undefined
+  }
+
+  cancel() {
+    this.editCar._id = undefined
+  }
+
+  save() {
+    this.parkingService.updateCar(this.editCar).subscribe(() => this.afterEnd())
+  }
+
+  add(car : Pojazd) {
+    this.parkingService.addCar(car).subscribe(car => {
+      if(car._id)
+        this.cars.push(car)
+    })
+  }
+
+  delete(car : Pojazd) {
+    this.cars = this.cars.filter(c => c !== car)
+    this.parkingService.deleteCar(car).subscribe()
   }
 
 }
--- src/cars/parking.service.ts
+++ src/cars/parking.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Parking, Pojazd } from './parking';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { catchError, map, tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,14 @@ export class ParkingService {
   private apiURL = 'https://web20.adamzielonka.pro/api/'
   private parkingURL = this.apiURL + 'parking/'
   private carsURL =  this.apiURL + 'car/'
+  private headers = new HttpHeaders()
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+    this.headers = this.headers.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjViMWJjNGYzMzI1MmQ1Mjk2MTJhY2UzNSIsInN1YiI6ImFkbWluIn0.rvjCthOwY0FA7mdoXq2LUOH7fuQGMh8IccRpvZJjNR0");
+    this.headers = this.headers.append('Content-Type','application/json')
+  }
 
   getParking() : Observable<Parking[]> {
     return this.http.get<Parking[]>(this.parkingURL)
@@ -26,5 +31,34 @@ export class ParkingService {
 
   getCar(id) : Observable<Pojazd> {
     return this.http.get<Pojazd>(this.carsURL + id)
+  }
+
+  updateCar(car : Pojazd) : Observable<any> {
+    return this.http.put(this.carsURL + car._id, car, { headers : this.headers }).pipe(
+      tap(_ => console.log(`aktualizacja pojazdu o id=${car._id}`)),
+      catchError(this.handleError<any>('updateCar'))
+    )
+  }
+
+  addCar(car : Pojazd): Observable<Pojazd> {
+    return this.http.post<Pojazd>(this.carsURL, car, { headers : this.headers }).pipe(
+      tap((car: Pojazd) => console.log(`dodanie pojazdu o id=${car._id}`)),
+      catchError(this.handleError<Pojazd>('addCar'))
+    )
+  }
+
+  deleteCar(car : Pojazd): Observable<Pojazd> {
+    return this.http.delete<Pojazd>(this.carsURL + car._id, { headers : this.headers }).pipe(
+      tap(_ => console.log(`deleted hero id=${car._id}`)),
+      catchError(this.handleError<Pojazd>('deletePojazd'))
+    )
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error)
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
   }
 }
