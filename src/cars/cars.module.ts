b85708019c43041b6b918cc6b6360260db223d6a
Adam Zielonka <root@adamzielonka.pro>
2018-06-09 11:15:56 +0200

    Deleted student components

--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -3,9 +3,6 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { CarsComponent } from './cars.component';
-import { StudentsComponent } from './students/students.component';
-import { StudentDetailComponent } from './student-detail/student-detail.component';
-import { StudentGradesComponent } from './student-grades/student-grades.component';
 import { PlaceComponent } from './place/place.component';
 import { PlaceDetailComponent } from './place-detail/place-detail.component';
 import { RegisterComponent } from './register/register.component';
@@ -13,9 +10,6 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
     CarsComponent,
-    StudentsComponent,
-    StudentDetailComponent,
-    StudentGradesComponent,
     PlaceComponent,
     PlaceDetailComponent,
     RegisterComponent
