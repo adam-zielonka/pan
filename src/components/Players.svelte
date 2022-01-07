27ec77371ba1428391060d32483e313dbabe0357
Adam Zielonka <root@adamzielonka.pro>
2022-01-07 17:15:10 +0100

    Delete old unused files

--- src/components/Players.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { store } from '../store/store'
-  import Player from './Player.svelte'
-  const { players } = store
-</script>
-
-{#each $players.players as player, i}
-  <Player id={i} {player} set={players.set} />
-{/each}
